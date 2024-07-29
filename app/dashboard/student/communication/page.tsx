00aebc5512143323797da42c63f8a360cecba0eb
markmumba <markmumba01@gmail.com>
2024-07-29 22:11:53 +0300

    following the errors

--- app/dashboard/student/communication/page.tsx
+++ app/dashboard/student/communication/page.tsx
@@ -1,11 +1,10 @@
-import ChatComponent from "@/app/UI/chat/chatpage";
 
 const CommunicationPage: React.FC = () => {
   return (
     <div className="h-screen flex flex-col">
       <h1 className="text-2xl font-bold p-4">Communication</h1>
       <div className="flex-grow">
-        <ChatComponent />
+        <div>The chat will be here </div>
       </div>
     </div>
   );
