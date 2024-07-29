eb852e61c5eb7962c335e12b1b5c1231fe4bb7be
markmumba <markmumba01@gmail.com>
2024-07-30 00:45:04 +0300

    removing the add credentials in the fetcher function

--- app/fetcher/fetcher.ts
+++ app/fetcher/fetcher.ts
@@ -6,7 +6,6 @@ export const axiosInstance = axios.create({
 
 const fetcher = async (url: string) => {
   const response = await axiosInstance.get(url, {
-    withCredentials: true,
     headers: { "Content-Type": "application/json" },
   });
   return response.data;
