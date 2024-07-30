51e6dfc414b8a89ef2c722d24fcf9fa15a43f64f
markmumba <markmumba01@gmail.com>
2024-07-30 15:15:36 +0300

    configure for prod

--- app/fetcher/fetcher.ts
+++ app/fetcher/fetcher.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL:  "http://localhost:8080" ,
+  baseURL:  process.env.NEXT_PUBLIC_BACKEND_API_URL || process.env.NEXT_BACKEND_API_URL ,
 });
 
 const fetcher = async (url: string) => {
